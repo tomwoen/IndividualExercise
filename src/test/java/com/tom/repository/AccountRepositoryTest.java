5c9a918645ffe6a18b05972054f4f8caf0a5a5e8
tomwoen <thomas.owen@qa.com>
2018-04-25 18:09:32 +0100

    "Started repository later"

---
+++ src/test/java/com/tom/repository/AccountRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.tom.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AccountRepositoryTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/tom/repository/JSONUtilTest.java
@@ -0,0 +1,20 @@
+package com.tom.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JSONUtilTest {
+	
+	private Account account;
+	
+
+	@Test
+	public void testGetJSONForObject() {
+		
+		
+		
+		assertEquals(expected,actual);
+	}
+
+}

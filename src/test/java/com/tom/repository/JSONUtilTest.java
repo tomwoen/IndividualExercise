63faaab2a7aced4f4251b6441b0c8f3ff90cd969
tmowen <thomas.owen@qa.com>
2018-04-25 21:52:31 +0100

    Added JSONUtilTest

--- src/test/java/com/tom/repository/JSONUtilTest.java
+++ src/test/java/com/tom/repository/JSONUtilTest.java
@@ -2,16 +2,23 @@ package com.tom.repository;
 
 import static org.junit.Assert.*;
 
+import javax.inject.Inject;
+
 import org.junit.Test;
+
+import com.tom.persistence.Account;
 
 public class JSONUtilTest {
 	
+	@Inject
 	private Account account;
 	
 
 	@Test
 	public void testGetJSONForObject() {
 		
+		
+		String actual = getJSONForObject(account ("Bill","Billson","1111"));
 		
 		
 		assertEquals(expected,actual);

d1494f1841bb7783a78fdbbace4556a482cb7436
tmowen <thomas.owen@qa.com>
2018-04-26 20:25:38 +0100

    Finished test for JSONUtil

--- src/test/java/com/tom/repository/JSONUtilTest.java
+++ src/test/java/com/tom/repository/JSONUtilTest.java
@@ -12,16 +12,25 @@ public class JSONUtilTest {
 	
 	@Inject
 	private Account account;
+	private Account expected;
+	private Account actual;
 	
 
 	@Test
 	public void testGetJSONForObject() {
 		
-		
-		String actual = getJSONForObject(account ("Bill","Billson","1111"));
-		
-		
+		account = new Account("Bill","Billson","1111");
+		String actual = getJSONForObject(account);
+		String expected = "{\"firstName\":\"Bill\",\"lastName\":\"Billson\",\"acountNumber\":\"1111\"}";		
 		assertEquals(expected,actual);
+	}
+	
+	@Test
+	public void testGetObjectForJSON() {
+		
+		expected = new Account("Phil", "Mitchell","1234");
+		actual = getObjectForJSON("{\"firstName\":\"Phil\",\"lastName\":\"Mitchel\",\"acountNumber\":\"1234\"}");
+		assertEquals(expected, actual);
 	}
 
 }
